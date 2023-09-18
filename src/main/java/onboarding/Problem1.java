4f6ab3b8be586764e628690daa235ac19cc14d39
LeeJieon <dlwldjs7544@naver.com>
2023-09-18 17:09:30 +0900

    feat: 페이지 번호의 각 자리 수를 더하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -39,4 +39,17 @@ class Problem1 {
             return false;
         }
     }
+
+    public static int addDigit(List<Integer> page) {
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            int digit = page.get(i);
+            while (digit > 0) {
+                sum += digit%10;
+                digit /= 10;
+            }
+        }
+
+        return sum;
+    }
 }
