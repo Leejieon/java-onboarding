15771c177f9c69a5ff8a0b3dc2af339500c8075d
LeeJieon <dlwldjs7544@naver.com>
2023-09-20 19:56:06 +0900

    feat: 연속하는 중복 문자를 확인하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,4 +5,32 @@ public class Problem2 {
         String answer = "answer";
         return answer;
     }
+
+    static String findDuplicateChar(String cryptogram) {
+        for (int start = 0; start < cryptogram.length() - 1; start++) {
+            boolean isDuplicate = false;
+            for (int end = start + 1; end < cryptogram.length(); end++) {
+                if (cryptogram.charAt(start) == cryptogram.charAt(end)) {
+                    isDuplicate = true;
+
+                    // end index 가 문자열 마지막에 도달했을 경우.
+                    if (end == cryptogram.length() - 1) {
+                        // TODO : 중복 문자 제거 메서드 추가
+                        cryptogram = removeDuplicateChar(cryptogram, start, end + 1);
+                    }
+                }
+                else {
+                    if (isDuplicate) {
+                        // TODO : 중복 문자 제거 메서드 추가
+                        cryptogram = removeDuplicateChar(cryptogram, start, end);
+                        start -= 1;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return cryptogram;
+    }
+
 }
