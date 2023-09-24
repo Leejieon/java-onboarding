503fc221d5c1a97780eb38c12fc9ebdd3b24e5da
Jieon <dlwldjs7544@naver.com>
2023-09-24 20:13:18 +0900

    feat: 숫자에 포함된 3, 6, 9의 개수를 반환하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,17 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int count369(int number) {
+        int count = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                count++;
+            }
+            number /= 10;
+        }
+
+        return count;
+    }
 }
